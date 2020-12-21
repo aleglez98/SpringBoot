5cbc2e304c57de48f7f51fd92bbd9c7fe14c01d9
Alejandro Gonzalez <aleglezglez98@gmail.com>
2020-12-21 10:22:30 +0100

    Commit Inicial

---
+++ Videojuegos/src/main/java/com/example/vdemo/controller/ListadoController.java
@@ -0,0 +1,15 @@
+package com.example.vdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class ListadoController {
+	
+	@RequestMapping("/")
+	public String listarVideojuegos() {
+		//Busqueda de videojuegos
+		return "listado";
+	}
+}
